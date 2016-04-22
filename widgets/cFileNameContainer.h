6d262a8bf93673c63671e18d7c9c635b962ef28f
colinporth <colinwrey@hotmail.com>
2016-04-22 13:05:59 +0100

    start of animation

--- widgets/cFileNameContainer.h
+++ widgets/cFileNameContainer.h
@@ -15,35 +15,31 @@ public:
 
   //{{{
   virtual void pressed (int16_t x, int16_t y) {
-    mMoved = false;
-    mMove = 0;
     mPressed = true;
     mPressedIndex = y / kBoxHeight;
+    mMoved = false;
+    mMove = 0;
+    mScrollInc = 0;
     }
   //}}}
   //{{{
   virtual void moved (int16_t x, int16_t y, int16_t z, int16_t xinc, int16_t yinc) {
+
     mMove += yinc;
     if (abs(mMove) > 2)
       mMoved = true;
 
-    if (mMoved) {
-      mScroll -= yinc;
-      if (mScroll < 0)
-        mScroll = 0;
-      else if (mScroll > (int(mFileNames.size()) - mLines) * kBoxHeight)
-       mScroll = (int(mFileNames.size()) - mLines) * kBoxHeight;
-
-      mFileNameFirstIndex = mScroll / kBoxHeight;
-      }
+    if (mMoved)
+      incScroll (yinc);
     }
   //}}}
   //{{{
   virtual void released() {
     if (!mMoved) {
-      mSelectedIndexRef = mFileNameFirstIndex + mPressedIndex;
+      mSelectedIndexRef = (int(mScroll) / kBoxHeight) + mPressedIndex;
       mChangedFlagRef = true;
       }
+
     mPressed = false;
     mPressedIndex = -1;
     mMoved = false;
@@ -52,31 +48,48 @@ public:
   //}}}
   //{{{
   virtual void draw (cLcd* lcd) {
-    int y = -(mScroll % kBoxHeight);
-    int index = mFileNameFirstIndex;
-    for (int i = 0; i < mLines; i++, index++) {
+
+    if (!mPressed && mScrollInc)
+      incScroll (mScrollInc / 1.1f);
+
+    int y = -(int(mScroll) % kBoxHeight);
+    int index = int(mScroll) / kBoxHeight;
+
+    for (int i = 0; i < mLines; i++, index++,y += kBoxHeight) {
       if (y >= 0) {
         if ((i == mPressedIndex) && mPressed && !mMoved)
           lcd->rectClipped (LCD_GREY, mX, mY+y+1, mWidth-1, kBoxHeight-1);
         lcd->string (index == mSelectedIndexRef ? LCD_WHITE : LCD_LIGHTGREY, lcd->getFontHeight(),
                      mFileNames[index], mX+2, mY+y+1, mWidth-1, mHeight-1);
         }
-      y += kBoxHeight;
       }
     }
   //}}}
 
 private:
+  //{{{
+  void incScroll (float inc) {
+
+    mScroll -= inc;
+    if (mScroll < 0.0f)
+      mScroll = 0.0f;
+    else if (mScroll > (int(mFileNames.size()) - mLines) * kBoxHeight)
+      mScroll = (int(mFileNames.size()) - mLines) * kBoxHeight;
+
+    mScrollInc = inc;
+    }
+  //}}}
+
   std::vector<std::string>& mFileNames;
   int& mSelectedIndexRef;
   bool& mChangedFlagRef;
-
-  int mScroll = 0;
-  int mFileNameFirstIndex = 0;
 
   bool mPressed = 0;
   int mPressedIndex = -1;
   bool mMoved = false;
   int mMove = 0;
+
   int mLines = 0;
+  float mScroll = 0;
+  float mScrollInc = 0.0f;
   };
