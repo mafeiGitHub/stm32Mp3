28b14d96e23dfac445553007d5fb1e8cec598a66
colinporth <colinwrey@hotmail.com>
2016-10-18 10:59:21 +0100

    more v1.5 hal, extern tables

--- sys/system_stm32f7xx.h
+++ sys/system_stm32f7xx.h
@@ -5,7 +5,9 @@
 #endif
 //}}}
 
-extern uint32_t SystemCoreClock;          /*!< System Clock Frequency (Core Clock) */
+extern uint32_t SystemCoreClock;
+extern const uint8_t AHBPrescTable[16];
+extern const uint8_t APBPrescTable[8];
 
 extern void SystemInit(void);
 extern void SystemCoreClockUpdate(void);
