dacdb27a98e174b26bffd07f9160ee86eaac0fb7
colinporth <colinwrey@hotmail.com>
2016-10-30 15:32:03 +0000

    bett irq routing

--- Bsp/stm32f7xx_it.c
+++ Bsp/stm32f7xx_it.c
@@ -36,30 +36,24 @@ void ETH_IRQHandler() { HAL_ETH_IRQHandler (&EthHandle); }
 void LTDC_IRQHandler() { LCD_LTDC_IRQHandler(); }
 void DMA2D_IRQHandler() { LCD_DMA2D_IRQHandler(); }
 
+// sd
+extern SD_HandleTypeDef uSdHandle;
+void BSP_SDMMC_IRQHandler() { HAL_SD_IRQHandler (&uSdHandle); }
+void BSP_SDMMC_DMA_Tx_IRQHandler() { HAL_DMA_IRQHandler (uSdHandle.hdmatx); }
+void BSP_SDMMC_DMA_Rx_IRQHandler() { HAL_DMA_IRQHandler (uSdHandle.hdmarx); }
+
 // audio
 extern SAI_HandleTypeDef haudio_out_sai;
 void AUDIO_OUT_SAIx_DMAx_IRQHandler() { HAL_DMA_IRQHandler (haudio_out_sai.hdmatx); }
 
-extern SD_HandleTypeDef uSdHandle;
-extern DMA_HandleTypeDef dma_rx_handle;
-extern DMA_HandleTypeDef dma_tx_handle;
+// audio in
 #ifdef STM32F746G_DISCO
-  // audio
   extern SAI_HandleTypeDef haudio_in_sai;
   void AUDIO_IN_SAIx_DMAx_IRQHandler() { HAL_DMA_IRQHandler (haudio_in_sai.hdmarx); }
-
-  // sd
-  void SDMMC1_IRQHandler() { HAL_SD_IRQHandler (&uSdHandle); }
-  void DMA2_Stream3_IRQHandler() { HAL_DMA_IRQHandler (&dma_rx_handle); }
-  void DMA2_Stream6_IRQHandler() { HAL_DMA_IRQHandler (&dma_tx_handle); }
 #else
-  // audio - reuses SD dma channels?
+  // audioin - reuses SD dma channels?
   extern DFSDM_Filter_HandleTypeDef hAudioInTopLeftFilter;
   extern DFSDM_Filter_HandleTypeDef hAudioInTopRightFilter;
   //void AUDIO_DFSDMx_DMAx_TOP_LEFT_IRQHandler() { HAL_DMA_IRQHandler (hAudioInTopLeftFilter.hdmaReg); }
   //void AUDIO_DFSDMx_DMAx_TOP_RIGHT_IRQHandler() { HAL_DMA_IRQHandler (hAudioInTopRightFilter.hdmaReg); }
-  // sd
-  void SDMMC2_IRQHandler() { HAL_SD_IRQHandler (&uSdHandle); }
-  void DMA2_Stream0_IRQHandler() { HAL_DMA_IRQHandler (&dma_rx_handle); }
-  void DMA2_Stream5_IRQHandler() { HAL_DMA_IRQHandler (&dma_tx_handle); }
 #endif
